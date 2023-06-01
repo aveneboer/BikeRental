69cf69d22607e24f8921a9152542042722387579
aveneboer <anoukveneboer@hotmail.com>
2023-06-01 12:05:10 +0200

    Alle relaties toegevoegd in de entiteitenklasse en getest in de database

--- src/main/java/nl/anouk/bikerental/models/Bike.java
+++ src/main/java/nl/anouk/bikerental/models/Bike.java
@@ -1,2 +1,28 @@
-package nl.anouk.bikerental.models;public class Bike {
+package nl.anouk.bikerental.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@DiscriminatorValue("BIKE")
+@Table(name = "bikes")
+
+public class Bike extends Vehicle {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "brands")
+    private String brand;
+    @Column(name = "sizes")
+    private String size;
+
+    @Column(name = "registration_numbers")
+    private Long registrationNo;
+
 }
--- src/main/java/nl/anouk/bikerental/models/Car.java
+++ src/main/java/nl/anouk/bikerental/models/Car.java
@@ -1,2 +1,26 @@
-package nl.anouk.bikerental.models;public class Car {
+package nl.anouk.bikerental.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@DiscriminatorValue("CAR")
+@Table(name = "cars")
+public class Car extends Vehicle {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "passengers")
+    private int passenger;
+
+
 }
