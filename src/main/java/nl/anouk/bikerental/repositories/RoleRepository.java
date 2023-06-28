147cddd8e27eab812bc5af358ad4dbd1713b6676
aveneboer <anoukveneboer@hotmail.com>
2023-06-28 11:21:08 +0200

    code opgeschoond

--- src/main/java/nl/anouk/bikerental/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package nl.anouk.bikerental.repositories;
-
-import nl.anouk.bikerental.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
-*/
